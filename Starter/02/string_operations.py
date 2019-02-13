284ad9b3c3c4ec35edad2301b7e1d777e2e62582
Andrew <cbb356@gmail.com>
2019-02-13 12:43:50 +0200

    String operations edited

--- Starter/02/string_operations.py
+++ Starter/02/string_operations.py
@@ -12,6 +12,6 @@ print(string1[2])
 print('See on strings: %s and %s' % (string1, string2))
 print('See on strings: {} and {}'.format(string1, string2))
 
-x = 5.123
+x = 5.14
 print('Number is %d' % (x))
 print('Number is {:f}'.format(x))
