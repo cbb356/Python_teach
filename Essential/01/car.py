ff0f8bc013237afe1d4474efa5d36f4533633403
Andrew <cbb356@gmail.com>
2019-02-25 13:35:14 +0200

    01 essential lesson exercises

--- Essential/01/car.py
+++ Essential/01/car.py
@@ -3,18 +3,32 @@ class Car:
         self.number = number
     def __repr__(self):
         return "Car #{}".format(self.number)
+    def __str__(self):
+        return "Car is {}".format(self.number)
+    def __eq__(self, other):
+        return self.number == other.number
 
 class Salon:
-    cars = [Car(1), Car(2), Car(3)]
+    cars = [Car(1), Car(2), Car(2), Car(3), Car(3)]
 
-    def sell_car(self, number):
+    def add_car(self, number):
+        self.cars.append(Car(number))
+
+    def sell_a_car(self, number):
         for i in self.cars:
             if i == Car(number):
-                del self.cars[i]
+                self.cars.remove(i)
+
+    def sell_all_cars(self, number):
+        while Car(number) in self.cars:
+            self.cars.remove(Car(number))
+
 
 salon = Salon()
 print(salon.cars)
-salon.sell_car(3)
+salon.add_car(4)
 print(salon.cars)
-
-    
+salon.sell_a_car(3)
+print(salon.cars)
+salon.sell_all_cars(2)
+print(salon.cars)
