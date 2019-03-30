3c95b16a2be26c135899472153e01cedb264752b
Andrew <cbb356@gmail.com>
2019-03-30 22:36:58 +0200

    03 essential

--- Essential/03/worker.py
+++ Essential/03/worker.py
@@ -5,9 +5,25 @@ class Worker:
         self.department = department
         self.year = year
 
+    def __str__(self):
+        return "Worker: " + self.first_name + " " + self.last_name + ", "\
+                + self.department + " department, since " + str(self.year)
 
+    @staticmethod
+    def read_worker():
+        first_name = input('Input first name:')
+        last_name = input('Input last name:')
+        department = input('Input name of department:')
+        year = input('Input year when the worker bevgun to work:')
+        return Worker(first_name, last_name, department, year)
+        
 def main():
-    worker = Worker()
-
+    worker = Worker.read_worker()
+    try:
+        print(worker)
+    except TypeError as error:
+        print("Some type error:", error)
+        
+        
 if __name__ == "__main__":
     main()
