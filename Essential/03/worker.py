f59f69055ed288391c900acdc928bacdb1d0cb94
Andrew <cbb356@gmail.com>
2019-05-02 09:47:56 +0300

    Essential 03

--- Essential/03/worker.py
+++ Essential/03/worker.py
@@ -1,5 +1,18 @@
+import datetime
+
 class Worker:
     def __init__(self, first_name, last_name, department, year):
+        if not first_name:
+            raise ValueError('first name cannot be empty')
+        if not first_name:
+            raise ValueError('last name cannot be empty')
+        if not first_name:
+            raise ValueError('department cannot be empty')
+        
+
+
+
+
         self.first_name = first_name
         self.last_name = last_name
         self.department = department
@@ -14,15 +27,28 @@ class Worker:
         first_name = input('Input first name:')
         last_name = input('Input last name:')
         department = input('Input name of department:')
-        year = input('Input year when the worker bevgun to work:')
+        year = int(input('Input year when the worker begun to work:'))
         return Worker(first_name, last_name, department, year)
+
+def found_year_f():
+    while True:
+        try:
+            found_year = int(input('Input year when company was founded:'))
+            break
+        except ValueError:
+            print('Enter correct year')
+    return found_year            
         
 def main():
-    worker = Worker.read_worker()
+    found_year = found_year_f()
+    workers = []
+    while True:
+        workers.append = Worker.read_worker()
     try:
         print(worker)
-    except TypeError as error:
-        print("Some type error:", error)
+    except ValueError as error:
+        print("Error:", error)
+    # print(found_year)
         
         
 if __name__ == "__main__":
