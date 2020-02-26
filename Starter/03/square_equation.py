a697c63fa0a1f91ff759d32fef3dce5f17cf38a7
Andrew <cbb356@gmail.com>
2020-02-26 14:53:02 +0200

    fix square equation in 03 starter lesson exercises

--- Starter/03/square_equation.py
+++ Starter/03/square_equation.py
@@ -4,12 +4,12 @@ c = float(input("Input c: "))
 
 d2 = (b**2 - 4*a*c)
 d = d2**0.5
-x1 = (-b + d)/2*a
-x2 = (-b - d)/2*a
+x1 = (-b + d)/(2*a)
+x2 = (-b - d)/(2*a)
 
 if d2 < 0:
     print('There are no regular roots')
-elif d == 0:
+elif d2 == 0:
     print('x = ', x1)
 else:
     print("x1 = {}, x2 = {}".format(x1, x2))
